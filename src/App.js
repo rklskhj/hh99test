e36f64fa731bef24a01c1d74bde267dd1c0f241b
rklskhj <rklskhj2@naver.com>
2022-10-06 11:24:50 +0900

    항해99 주특기 1주차 테스트

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "react를 배워봅시다.",
+    },
+  ]);
+
+  return (
+    <Layout>
+      <Form todos={todos} setTodos={setTodos}></Form>
+      <List todos={todos} setTodos={setTodos}></List>
+    </Layout>
+  );
+}
+
+function Layout(props) {
+  return <div className="layout">{props.children}</div>;
+}
+
+function Form({ setTodos, todos }) {
+  const init = {
+    title: "",
+  };
+
+  const [todo, setTodo] = useState(init);
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const onSubmit = () => {
+    if (todo.title.trim() === "") return;
+    setTodos([...todos, { ...todo }]);
+    setTodo(init);
+  };
+  // console.log(todos);
+
+  return (
+    <div className="container">
+      <div className="input-box">
+        <input
+          type="text"
+          name="title"
+          value={todo.title}
+          onChange={onChange}
+        ></input>
+        <button onClick={onSubmit}>추가하기</button>
+      </div>
+    </div>
+  );
+}
+
+function List({ setTodo, todos }) {
+  console.log(todos);
+  return (
+    <div className="todo-list">
+      <h1>Todo List</h1>
+      <div className="list-wrapper">
+        {todos.map((todo) => (
+          <Todo todo={todo} setTodo={setTodo}></Todo>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Todo({ todo }) {
+  return (
+    <div className="todo-box">
+      <h2>{todo.title}</h2>
+    </div>
+  );
+}
+
+export default App;
